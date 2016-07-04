48acfcac08ab2b00cb53a30405fefa7011fbe46e
Nagendra Prasad <wowmylibrary@gmail.com>
2016-07-04 13:02:03 +0530

    1.2 passed, 1.3 Test Added

---
+++ code-school/1.2/hello-you.js
@@ -0,0 +1,12 @@
+﻿/*
+1. First, tell the response which status it should have (a successful status is 200).
+2. Next, write a message to the response body in the form of "Hello, this is <your name here>".
+3. To finish it up, tell the response to end so the client on the other side knows it has received all the data.
+*/
+
+var http = require('http');
+http.createServer(function (request, response) {
+    response.writeHead(200);//Status code in header 
+    response.write("Hello, this is nagendra");//Reponse body
+    response.end();//Close the connection
+}).listen(8080);
---
+++ code-school/1.3/convert-blocking.js
@@ -0,0 +1,5 @@
+﻿var fs = require('fs');
+
+var contents = fs.readFileSync('index.html');
+
+console.log(contents);
